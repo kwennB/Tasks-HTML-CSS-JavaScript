c2af3ebdfb73c48d15b76a36afd8b71e0802c2c5
kwennB <60541622+kwennB@users.noreply.github.com>
2020-03-24 16:39:46 +0100

    first commit to this respository

---
+++ First-Web-Page/scipt.js
@@ -0,0 +1,12 @@
+var myName ="Anyebe Blessing Ene";
+var myCourses = ["HTML", "CSS", "Javascript", "UX/UI Design"];
+console.log(myName);
+console.log(myCourses);
+
+for (var i=0; <= 220; i++)
+{
+    if(i % 2 ===0)
+    {
+        console.log(i);
+    }
+}
